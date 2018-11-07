1f7ad4f689cc16759670194400c40df097b62e60
TamilaTE <tamilaaghdami@yahoo.com>
2018-11-06 23:33:00 -0800

    first commit/working app

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# **FRIEND FINDER** 
+### If you take a survey the app will find you a friend with same interests of you. The app compares your answers with the answers of other people who took the survey.
+## **Technologies used**
+- Node.js
+- body-parager NPM Package 
+- express NPM Package
+- path NPM Package
+## **How to run the app?**
+- npm init
+- npm install 
+- make sure you have the dependencies (body-parager, path, express)
+- in the console run node server.js
+- Open localhost: 8080 in your browser
+### [Check it out!](https://tamilae.github.io/GIPHY-API/).
+
---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        name: "Shaggy",
+        photo: "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Shaggy/characterArt-shaggy-SD.png",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+
+    {
+        name: "Scooby doo",
+        photo: "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Scooby/characterArt-scooby-SD.png",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+
+    {
+        name: "Velma",
+        photo: "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Velma/characterArt-velma-SD.png",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+
+    {
+        name: "Fred",
+        photo: "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Fred/characterArt-velma-SD.png",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+
+    
+    {
+        name: "Daphne",
+        photo: "https://www.wbkidsgo.com/Portals/4/Images/Content/Characters/Velma/characterArt-velma-SD.png",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+
+];
+
+module.exports = friends;
